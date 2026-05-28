c18c096642a43d7548ab4da988f654c817340a9c
thepieter22-web <thepieter22@gmail.com>
2026-05-28 13:09:47 +0200

    Update i18n.ts

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,7 +1,7 @@
-import {getRequestConfig} from 'next-intl/server';
-import {notFound} from 'next/navigation';
+import { getRequestConfig } from 'next-intl/server';
+import { notFound } from 'next/navigation';
 
-export default getRequestConfig(async ({locale}) => {
+export default getRequestConfig(async ({ locale }) => {
   if (locale !== 'nl') notFound();
 
   return {
