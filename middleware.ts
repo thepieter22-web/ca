917df7b10f7ad0006a2408bf9fff36d460539515
thepieter22-web <thepieter22@gmail.com>
2026-05-28 13:05:46 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,10 @@
 import createMiddleware from 'next-intl/middleware';
 
-export
+export default createMiddleware({
+  locales: ['nl'],
+  defaultLocale: 'nl'
+});
+
+export const config = {
+  matcher: ['/', '/nl/:path*']
+};
