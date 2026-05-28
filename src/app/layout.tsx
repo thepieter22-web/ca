2be4851b1628683d1da1d32011a9f9cc3f93abe3
thepieter22-web <thepieter22@gmail.com>
2026-05-28 12:49:53 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,5 +11,9 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return children;
+  return (
+    <html lang="nl">
+      <body>{children}</body>
+    </html>
+  );
 }
